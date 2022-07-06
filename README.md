96f10b2d47a267b80cbbeeae50f6acae3176911c
Ricardo Vieira <ricovieira26@gmail.com>
2022-07-06 00:20:57 -0300

    Inicio do projeto

---
+++ README.md
@@ -0,0 +1,62 @@
+# **Cloneflix** ![](https://img.shields.io/badge/Progresso-1%25-green)
+
+------
+
+```php
+/*
+Projeto para recriar uma plataforma de stream de vídeos codificada em PHP sem utilização de frameworks. Com o objetivo de aperfeiçoar conhecimentos em Front-end e Back-end. Lembrando que não é o objetivo do projeto ter uma representação fiel da referência.
+*/
+```
+
+## 💻**Desenvolvimento e melhorias**
+
+------
+
+***Design***
+
+- [ ] Layout da página inicial
+- [ ] Responsividade da página inicial
+- [ ] Login e cadastro
+- [ ] Página de navegação
+- [ ] Página do filme selecionado
+- [ ] Player
+- [ ] Páginas de perfis e configurações
+- [ ] Página de ajuda
+
+***Programação***
+
+- [ ] Banco de dados
+- [ ] Login e cadastro
+- [ ] Perfil e configurações
+- [ ] Listagem de conteúdo
+- [ ] Sistema de assinaturas
+- [ ] Múltiplos idiomas
+- [ ] Demais ajustes
+
+## ⚙**Linguagens e ferramentas**
+
+------
+
+***Linguagens***
+
+- HTML5
+- CSS
+- JavaScript
+- PHP
+
+***Banco de dados***
+
+- MySQL
+
+***Editor***
+
+Visual Studio Code
+
+------
+
+## 📝**Licença**
+
+Esse projeto está sob licença. Veja o arquivo [LICENÇA](LICENSE.md) para mais detalhes.
+
+
+
---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Netflix Brasil - assistir a séries online, assistir a filmes online</title>
+    <meta content="filme, assistir online, assistir TV, TV online, séries, séries de TV online, séries online, ver filmes, filmes streaming, TV streaming, séries streaming, assistir online, assistir filmes Brasil, filmes sem download, temporada completa série, longas, assistir a longas" name="keywords">
+    <meta content="Assista a séries e filmes online diretamente na sua Smart TV, PC ou Mac, videogame, tablet, smartphone e mais." name="description">
+    <link rel="shortcut icon" href="assets/img/netflix.ico">
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="logo">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 111 30" focusable="false">
+                <g>
+                    <path d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.2498828 40.6561065,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z" fill="#e50914"></path>
+                </g>
+            </svg>
+        </div>
+        <div class="box_opt">
+            <div class="lang_selector">
+                <span class="globe"></span>
+                <select name="" class="lang_select">
+                    <option value="" class="option">Português</option>
+                    <option value="">Inglês</option>
+                </select>
+            </div>
+            <div class="singin">
+                <a href="#">Entrar</a>
+            </div>
+        </div>
+    </div>
+    <div class="main">
+        <div class="main_child">
+            <h1>
+                Filmes, séries e muito mais. Sem limites.
+            </h1>
+            <h2>
+                Assista onde quiser. Cancele quando quiser.
+            </h2>
+            <p>
+                Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.
+            </p>
+            <form action="#" class="form_cta">
+                <input type="text" placeholder="Email">
+                <button type="submit">
+                    <span class="btn_txt">Vamos lá</span>
+                    <span class="right_arrow">
+                        <svg viewBox="0 0 6 12" xmlns="http://www.w3.org/2000/svg">
+                            <desc>chevron</desc>
+                            <path d="M.61 1.312l.78-.624L5.64 6l-4.25 5.312-.78-.624L4.36 6z" fill="#fff"></path>
+                        </svg>
+                    </span>
+                </button>
+            </form>
+        </div>
+        <div class="gradient"></div>
+    </div>
+    <div class="section tv">
+        <div class="container">
+            <div class="grid">
+                <div>
+                    <h1>
+                        Aproveite na TV.
+                    </h1>
+                    <p>
+                        Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray e outros dispositivos.
+                    </p>
+                </div>
+                <div class="img_tv">
+                    <img src="assets/img/tv1.png" alt="TV1">
+                    <video class="video_tv" autoplay playsinline muted loop>
+                        <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="container">
+            <div class="grid_reverse">
+                <div class="mobile">
+                    <h1>
+                        Baixe séries para assistir offline.
+                    </h1>
+                    <p>
+                        Salve seus títulos favoritos e sempre tenha algo para assistir.
+                    </p>
+                </div>
+                <div class="img_mobile">
+                    <img src="assets/img/mobile-image.jpg" alt="mobile">
+                    <div class="box_download">
+                        <div class="box_card">
+                            <img src="assets/img/boxshot.png">
+                        </div>
+                        <div class="texts">
+                            <div class="text1">
+                                Stranger Things
+                            </div>
+                            <div class="text2">
+                                Download em andamento...
+                            </div>
+                        </div>
+                        <div class="download_ani"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section devices">
+        <div class="container">
+            <div class="grid">
+                <div>
+                    <h1>
+                        Assista quando quiser.
+                    </h1>
+                    <p>
+                        Assista no celular, tablet, Smart TV ou notebook sem pagar a mais por isso.
+                    </p>
+                </div>
+                <div class="img_tv">
+                    <img src="assets/img/tv2.png" alt="TV2">
+                    <video class="video_tv" autoplay playsinline muted loop>
+                        <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="container">
+            <div class="grid_reverse">
+                <div class="texts_kids">
+                    <h1>
+                        Crie perfis para crianças.
+                    </h1>
+                    <p>
+                        Deixe as crianças se aventurarem com seus personagens favoritos em um espaço feito só para elas, sem pagar a mais por isso.
+                    </p>
+                </div>
+                <div class="img_kids">
+                    <img src="assets/img/kids.png" alt="Kids">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="form_footer">
+            <p>
+                Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.
+            </p>
+            <form action="#" class="form_cta">
+                <input type="text" placeholder="Email">
+                <button type="submit">
+                    <span class="btn_txt">Vamos lá</span>
+                    <span class="right_arrow">
+                        <svg viewBox="0 0 6 12" xmlns="http://www.w3.org/2000/svg">
+                            <desc>chevron</desc>
+                            <path d="M.61 1.312l.78-.624L5.64 6l-4.25 5.312-.78-.624L4.36 6z" fill="#fff"></path>
+                        </svg>
+                    </span>
+                </button>
+            </form>
+        </div>
+    </div>
+    <div class="footer">
+        <p class="tell">Dúvidas? Ligue 0800 591 8942</p>
+        <ul class="structural">
+            <li class="footer-link-item"><a href="#">Perguntas frequentes</a></li>
+            <li class="footer-link-item"><a href="#">Centro de ajuda</a></li>
+            <li class="footer-link-item"><a href="#">Conta</a></li>
+            <li class="footer-link-item"><a href="#">Media Center</a></li>
+            <li class="footer-link-item"><a href="#">Relações com investidores</a></li>
+            <li class="footer-link-item"><a href="#">Carreiras</a></li>
+            <li class="footer-link-item"><a href="#">Resgatar cartão pré-pago</a></li>
+            <li class="footer-link-item"><a href="#">Comprar cartão pré-pago</a></li>
+            <li class="footer-link-item"><a href="#">Formas de assistir</a></li>
+            <li class="footer-link-item"><a href="#">Termos de uso</a></li>
+            <li class="footer-link-item"><a href="#">Privacidade</a></li>
+            <li class="footer-link-item"><a href="#">Preferências de cookies</a></li>
+            <li class="footer-link-item"><a href="#">Informações corporativas</a></li>
+            <li class="footer-link-item"><a href="#">Entre em contato</a></li>
+            <li class="footer-link-item"><a href="#">Teste de velocidade</a></li>
+            <li class="footer-link-item"><a href="#">Avisos legais</a></li>
+            <li class="footer-link-item"><a href="#">Só na Netflix</a></li>
+        </ul>
+        <p class="copy">Netflix Brasil</p>
+    </div>
+</body>
+
+</html>
